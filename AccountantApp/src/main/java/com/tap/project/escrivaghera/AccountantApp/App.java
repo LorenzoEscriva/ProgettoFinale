7546e7342c2f0c144b995286a3066ba914370e29
lorenzo <lorenzo@lore>
2016-12-22 11:20:37 +0100

    New-project

---
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/App.java
@@ -0,0 +1,13 @@
+package com.tap.project.escrivaghera.AccountantApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
