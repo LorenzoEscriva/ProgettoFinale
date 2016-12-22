e4eb6109166111820a78e7bd88ee6b1feb66dc81
lorenzo <lorenzo@localhost>
2016-12-22 15:37:38 +0100

    reformat-code

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/App.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/App.java
@@ -4,10 +4,8 @@ package com.tap.project.escrivaghera.AccountantApp;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
