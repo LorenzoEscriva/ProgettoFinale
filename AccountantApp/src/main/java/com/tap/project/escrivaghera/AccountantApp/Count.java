8d3745e459b3d761d0488202bbe5c9ea7212a812
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-23 18:21:40 +0100

    add class Count

---
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/Count.java
@@ -0,0 +1,19 @@
+package com.tap.project.escrivaghera.AccountantApp;
+
+public class Count {
+
+	
+	String description;
+	boolean isLeft;
+	double value;
+
+	public Count(String description, boolean isLeft) {
+		this.description = description;
+		this.isLeft = isLeft;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
+
+}
