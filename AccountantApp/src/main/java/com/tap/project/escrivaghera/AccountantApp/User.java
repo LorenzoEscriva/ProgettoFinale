a326e6cd24ebaab261c09ab961acaf4b86d9417c
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-23 18:34:10 +0100

    add class User

---
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/User.java
@@ -0,0 +1,14 @@
+package com.tap.project.escrivaghera.AccountantApp;
+
+public class User {
+	private String id;
+	private String name;
+	private String surname;
+	
+	public User(String id, String name, String surname) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+	}	
+}
