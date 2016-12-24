8776b557ecc030be7539a09d508b0ca8049188f3
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-24 12:07:52 +0100

    add toString in class Session and verify format
    Conflicts:
    	AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/Session.java
    	AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/SessionTest.java

---
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/Session.java
@@ -0,0 +1,39 @@
+package com.tap.project.escrivaghera.AccountantApp;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+public class Session {
+	private int id;
+	private User user;
+	private List<String> userActions; 
+	private final static Logger LOGGER =Logger.getLogger(Session.class);
+
+	public Session(int id, User user) {
+		this.id=id;
+		this.user=user;
+		userActions=new ArrayList<String>();
+	}
+
+	public void addDescription(String action) {
+		String actionDescription=new String("In the session "+ id+ ", the user "+ user.toString() +" "+ action);
+		LOGGER.info(actionDescription);
+		userActions.add(actionDescription);
+	}
+
+	public List<String> getList() {
+		return userActions;
+	}
+	
+	public String toString(){
+		String sessionDescription = "The user " + user.toString() + " makes this actions:\n";
+		Iterator<String> actionit=userActions.iterator();
+		while(actionit.hasNext()){
+			sessionDescription=sessionDescription+actionit.next()+ "\n";
+		}
+		return sessionDescription;
+	}
+}
