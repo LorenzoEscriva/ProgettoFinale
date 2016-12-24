60b050240ad75363abe4ed0c16bbb5b556166c5d
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-24 11:36:47 +0100

    add test description format

--- AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/SessionTest.java
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/SessionTest.java
@@ -7,18 +7,26 @@ import org.junit.Test;
 
 public class SessionTest {
 	private Session mysession;
+	private User myuser;
 
 	@Before
 	public void setUp() throws Exception {
-		User myuser=new User("1","test", "test");
+		myuser=new User("1","test", "test");
 		mysession=new Session(1, myuser);
 	}
 
 	@Test
-	public void testAddADescriptionToActionList() {
+	public void testAddDescriptionToActionList() {
 		String action=new String("delete record with id 0001");
 		mysession.addDescription(action);
 		assertEquals(1, mysession.getList().size());
 	}
 
+	@Test
+	public void testAddDescriptionCorrectDescriptionFormat(){
+		String action=new String("delete record with id 0001");
+		String descriptionAction=("In the session 1, the user "+ myuser.toString() +" "+ action);
+		mysession.addDescription(action);
+		assertEquals(descriptionAction, mysession.getList().get(0));
+	}
 }
