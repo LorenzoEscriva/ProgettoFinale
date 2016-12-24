9d100b102848c0b006c2f593d5d53316c0b9ec7b
lorenzo <lorenzo@lore>
2016-12-24 13:11:30 +0100

    create-new-branch-and-default-test-class

---
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/JournalEntryTest.java
@@ -0,0 +1,19 @@
+package com.tap.project.escrivaghera.AccountantApp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JournalEntryTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
