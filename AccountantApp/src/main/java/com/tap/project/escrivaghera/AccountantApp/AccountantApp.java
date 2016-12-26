b3509e8d5b172792a962f9b1a9cf578a8018549e
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-26 16:40:41 +0100

    test createJournalEntry in bad list of counts

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/AccountantApp.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/AccountantApp.java
@@ -1,5 +1,9 @@
 package com.tap.project.escrivaghera.AccountantApp;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class AccountantApp {
 	private Session mySession;
 	private Database myDB;
@@ -17,6 +21,12 @@ public class AccountantApp {
 
 	public void authenticate(User anUser) {
 		mySession=myServer.getSession(anUser);
+	}
+
+	public JournalEntry createJournalEntry(String string, Date date, List<Count> arrayList) {
+		if(mySession==null)
+			throw new NotAuthenticationException();
+		return null;
 	}
 
 }
--- AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/AccountantAppTest.java
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/AccountantAppTest.java
@@ -3,29 +3,53 @@ package com.tap.project.escrivaghera.AccountantApp;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class AccountantAppTest {
 	private AccountantApp myAccountantApp;
 	private Database db;
+	private User user;
 
 	@Before
 	public void setUp() throws Exception {
 		db=mock(Database.class);
 		Server serverTest=new Server(db);
 		myAccountantApp=new AccountantApp(serverTest);
+		user = new User("1", "test", "test");
 	}
 
 	@Test
 	public void testAuthenticatedMethod() {
-		User user =new User("1", "test", "test");
 		myAccountantApp.authenticate(user);
 		assertNotNull(myAccountantApp.getMySession());
 	}
 	
 	@Test(expected=NotAuthenticationException.class)
 	public void testCreateJournalEntryWithoutAuthentication(){
-		
+		myAccountantApp.createJournalEntry("1", new Date(), new ArrayList<Count>());
 	}
+	
+	@Test
+	public void testCreateJournalEntryWithBadListOfCounts(){
+		myAccountantApp.authenticate(user);
+		ArrayList<Count> myCounts = createTestList(1200.0, 1100.0);
+		assertNull(myAccountantApp.createJournalEntry("1", new Date(), myCounts));
+	}
+
+	public ArrayList<Count> createTestList(double leftValue, double rightValue) {
+		ArrayList<Count> myCounts=new ArrayList<Count>();
+		Count count=new Count("count1", true);
+		count.setValue(leftValue);
+		myCounts.add(count);
+		count=new Count("count2", false);
+		count.setValue(rightValue);
+		myCounts.add(count);
+		return myCounts;
+	}
+	
+	
 }
