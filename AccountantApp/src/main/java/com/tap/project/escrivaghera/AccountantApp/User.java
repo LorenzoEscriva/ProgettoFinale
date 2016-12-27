3b720b9ac9c3dfa1b96745e1fc75c60d51890d36
lorenzo <lorenzo@localhost>
2016-12-27 15:41:11 +0100

    reformat-code

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/User.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/User.java
@@ -1,18 +1,19 @@
 package com.tap.project.escrivaghera.AccountantApp;
 
 public class User {
+	
 	private String id;
 	private String name;
 	private String surname;
-	
+
 	public User(String id, String name, String surname) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.surname = surname;
 	}
-	
-	public String toString(){
-		return id+ " "+ name+ " "+ surname;
+
+	public String toString() {
+		return id + " " + name + " " + surname;
 	}
 }
