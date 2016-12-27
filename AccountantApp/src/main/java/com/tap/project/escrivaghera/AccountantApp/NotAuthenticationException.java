da65b4f872fbae7becd8abdbddb9cd8803fa78fc
lorenzo <lorenzo@localhost>
2016-12-27 15:41:11 +0100

    reformat-code

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/NotAuthenticationException.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/NotAuthenticationException.java
@@ -1,7 +1,8 @@
 package com.tap.project.escrivaghera.AccountantApp;
 
-public class NotAuthenticationException extends RuntimeException{
-	public NotAuthenticationException(){
+public class NotAuthenticationException extends RuntimeException {
+
+	public NotAuthenticationException() {
 		super("Authentication missing");
 	}
 }
