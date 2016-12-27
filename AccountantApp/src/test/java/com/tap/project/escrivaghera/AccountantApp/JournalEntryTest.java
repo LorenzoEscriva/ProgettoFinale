1da351cca6c8546798130d9ac225cfb1f2c91696
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-27 22:40:00 +0100

    change ambiguous test of exception in JournalEntryTest

--- AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/JournalEntryTest.java
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/JournalEntryTest.java
@@ -35,8 +35,8 @@ public class JournalEntryTest {
 		assertEquals(list, myJournalEntry.getListOfCount());
 	}
 
-	@Test(expected = IllegalJournalEntryException.class)
-	public void setListOfCountTestExceptionCase() throws IllegalJournalEntryException {
+	@Test
+	public void setListOfCountTestExceptionCase() {
 		List<Count> list = new ArrayList<Count>();
 		Count count1 = new Count("count1", true);
 		Count count2 = new Count("count2", false);
@@ -44,6 +44,11 @@ public class JournalEntryTest {
 		count2.setValue(1100);
 		list.add(count1);
 		list.add(count2);
-		myJournalEntry.setListOfCount(list);
+		try {
+			myJournalEntry.setListOfCount(list);
+			fail("should not go here");
+		} catch (IllegalJournalEntryException e) {
+			assertTrue(e.getLocalizedMessage().equals("The total of counts is several, the difference between the left count and the right count is: 100.0"));
+		}
 	}
 }
