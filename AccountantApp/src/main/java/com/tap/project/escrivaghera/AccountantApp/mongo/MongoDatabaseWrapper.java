8407c7b43de08a4bff65a8b2216bb29a33c7f956
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-28 15:49:50 +0100

    add classes

---
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/mongo/MongoDatabaseWrapper.java
@@ -0,0 +1,35 @@
+package com.tap.project.escrivaghera.AccountantApp.mongo;
+
+import java.util.Date;
+import java.util.List;
+
+import com.tap.project.escrivaghera.AccountantApp.Database;
+import com.tap.project.escrivaghera.AccountantApp.JournalEntry;
+
+public class MongoDatabaseWrapper implements Database {
+
+	@Override
+	public void add(JournalEntry newEntry) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modify(String id, JournalEntry changeEntry) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(String id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<JournalEntry> getAllRegistration(Date date1, Date date2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/mongo/MongoDatabaseWrapperTest.java
@@ -0,0 +1,20 @@
+package com.tap.project.escrivaghera.AccountantApp.mongo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MongoDatabaseWrapperTest {
+	private MongoDatabaseWrapper mongoDatabase;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
