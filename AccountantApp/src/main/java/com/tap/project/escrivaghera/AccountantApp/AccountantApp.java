c501e4a5e4c2d51b74dd47693fb208156490e1d2
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-28 10:01:26 +0100

    change message log in the AccountantApp class for exception

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/AccountantApp.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/AccountantApp.java
@@ -35,7 +35,7 @@ public class AccountantApp {
 		try {
 			newJournalEntry.setListOfCount(listOfCounts);
 		} catch (IllegalJournalEntryException e) {
-			LOGGER.error("Exception happen!", e);
+			LOGGER.error("EXCEPTION "+e.getMessage());
 			return null;
 		}
 		return newJournalEntry;
