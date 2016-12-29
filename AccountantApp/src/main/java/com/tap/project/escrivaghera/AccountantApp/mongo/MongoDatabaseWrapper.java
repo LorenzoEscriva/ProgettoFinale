3193cafceb882abb8d41da6226ce8470dc13c114
Matteo Ghera <matteo.ghera@stud.unifi.it>
2016-12-29 10:20:13 +0100

    modify some things and add comment to getAllRegistraction methods

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/mongo/MongoDatabaseWrapper.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/mongo/MongoDatabaseWrapper.java
@@ -1,6 +1,6 @@
 package com.tap.project.escrivaghera.AccountantApp.mongo;
 
-import java.text.DateFormat;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -54,6 +54,14 @@ public class MongoDatabaseWrapper implements Database {
 		return numberOfRecordsDelete-1;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * @see com.tap.project.escrivaghera.AccountantApp.Database#getAllRegistration(java.util.Date, java.util.Date)
+	 * 
+	 * For implement the getAllRegistraction between two date we take the list of all registrations
+	 * (with the getAllRegistraction method without parameters) and then we delete from that list
+	 * the intruders element, that is the records that have got the date out of range.
+	 */
 	@Override
 	public List<JournalEntry> getAllRegistration(Date date1, Date date2) {
 		List<JournalEntry> listOfJournalEntry=getAllRegistration();
@@ -69,28 +77,41 @@ public class MongoDatabaseWrapper implements Database {
 		}
 		return listOfJournalEntry;
 	}
-
+	
+	/*
+	 * For create the list of JounrnalEntry's object we need to create the JournalEntries's object
+	 * from the database info. We note that the record format is:
+	 * |   id|   date|  description|   value|  isLeft|
+	 * First of all, we take the info for create a JounalEntry object using the class constructor
+	 * In the second part, we take the info from the record for create the first Count and we add
+	 * it to the list of Count object
+	 * Finally, we create all other Count object analyzing the subsequent record that have got equal
+	 * id. We put the objects create into the corresponding list
+	 */
 	private List<JournalEntry> getAllRegistration() {
 		List<JournalEntry> listOfJournalEntry=new ArrayList<JournalEntry>();
 		DBCursor cursor=accountingRecords.find();
-		List<DBObject> myCursorIterator=cursor.toArray();
+		List<DBObject> myCursorList=cursor.toArray();
 		int i=0;
-		while(i<myCursorIterator.size()){
-			DBObject newJournalEntryCursor=myCursorIterator.get(i);
+		while(i<myCursorList.size()){
+			//we create the JournalEntry object
+			DBObject newJournalEntryCursor=myCursorList.get(i);
 			String idJournalEntry=(String)newJournalEntryCursor.get("id");
 			String dateJournalEntry=(String)newJournalEntryCursor.get("date");
 			try {
+				//we create the first Count's object (the current record's Count object)
 				Date date = convertStringToDate(dateJournalEntry);
 				JournalEntry newEntry=new JournalEntry(idJournalEntry, date);
 				
 				List<Count> counts =new ArrayList<Count>();
 				Count count = extractCount(newJournalEntryCursor);
 				counts.add(count);
-
+				
+				//we create the other Count object from the subsequent records info
 				boolean exit=false;
-				while(i+1<myCursorIterator.size() && !exit){
+				while(i+1<myCursorList.size() && !exit){
 					i=i+1;
-					DBObject newCountCursor=myCursorIterator.get(i);
+					DBObject newCountCursor=myCursorList.get(i);
 					String idNextRecord=(String)newCountCursor.get("id");
 					if(idNextRecord.equals(idJournalEntry)){
 						count=extractCount(newCountCursor);
