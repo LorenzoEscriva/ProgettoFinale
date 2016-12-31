e76d1184b1b67b3b9d73d096ed7803becdb9a73f
lorenzo <lorenzo@lore>
2016-12-31 16:58:53 +0100

    refactor-AccountantIT-class

--- AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/IT/AccountantAppIT.java
+++ AccountantApp/src/test/java/com/tap/project/escrivaghera/AccountantApp/IT/AccountantAppIT.java
@@ -1,6 +1,8 @@
 package com.tap.project.escrivaghera.AccountantApp.IT;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -11,109 +13,102 @@ import org.junit.Test;
 import com.github.fakemongo.Fongo;
 import com.mongodb.MongoClient;
 import com.tap.project.escrivaghera.AccountantApp.AccountantApp;
+import com.tap.project.escrivaghera.AccountantApp.Database;
 import com.tap.project.escrivaghera.AccountantApp.JournalEntry;
 import com.tap.project.escrivaghera.AccountantApp.Server;
 import com.tap.project.escrivaghera.AccountantApp.User;
+import com.tap.project.escrivaghera.AccountantApp.exception.IllegalJournalEntryException;
 import com.tap.project.escrivaghera.AccountantApp.helper.GenericHelper;
+import com.tap.project.escrivaghera.AccountantApp.helper.MongoTestHelper;
 import com.tap.project.escrivaghera.AccountantApp.mongo.MongoDatabaseWrapper;
 
 public class AccountantAppIT {
 
+	private MongoTestHelper mongoTestHelper;
 	private AccountantApp myAccountantApp;
 	private User myUser;
-	private MongoDatabaseWrapper myDB;
 
 	@Before
 	public void setUp() throws Exception {
-		Fongo fongo = new Fongo("mongo server");
+		Fongo fongo = new Fongo("Server");
 		MongoClient mongoClient = fongo.getMongo();
-		myDB = new MongoDatabaseWrapper(mongoClient);
+		mongoTestHelper = new MongoTestHelper(mongoClient);
+		Database myDB = new MongoDatabaseWrapper(mongoClient);
 		Server serverTest = new Server(myDB);
 		myAccountantApp = new AccountantApp(serverTest);
 		myUser = new User("1", "testIT", "testIT");
 	}
 
 	@Test
-	public void testAddIT() {
-		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1100, 1100));
-		myDB.add(testEntry);
-		assertEquals(1, myDB.getAllRegistration(dates[0], dates[1]).size());
+	public void testAddIT() throws IllegalJournalEntryException {
+		myAccountantApp.authenticate(myUser);
+		JournalEntry testEntry = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		myAccountantApp.add(testEntry);
+		assertTrue(mongoTestHelper.containRecord(testEntry));
 	}
 
 	@Test
-	public void testModifyIT() {
-		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry1 = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1100, 1100));
-		JournalEntry testEntry2 = myAccountantApp.createJournalEntry("2", dates[0],
-				GenericHelper.createTestList(1200, 1200));
-		myDB.add(testEntry1);
-		myDB.modify("1", testEntry2);
-		assertEquals(1, myDB.getAllRegistration(dates[0], dates[1]).size());
+	public void testModifyIT() throws IllegalJournalEntryException {
+		myAccountantApp.authenticate(myUser);
+		JournalEntry testEntry1 = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		JournalEntry testEntry2 = mongoTestHelper.createJournalEntry("1", 1200, 1200);
+		mongoTestHelper.addRecord(testEntry1);
+		myAccountantApp.modify("1", testEntry2);
+		assertFalse(mongoTestHelper.containRecord(testEntry1));
+		assertTrue(mongoTestHelper.containRecord(testEntry2));
 	}
 
 	@Test
-	public void testDeleteIT() {
-		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry = myAccountantApp.createJournalEntry("4", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		myDB.add(testEntry);
-		myDB.delete("4");
-		assertEquals(0, myDB.getAllRegistration(dates[0], dates[1]).size());
+	public void testDeleteIT() throws IllegalJournalEntryException {
+		myAccountantApp.authenticate(myUser);
+		JournalEntry removed = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		mongoTestHelper.addRecord(removed);
+		myAccountantApp.delete("1");
+		assertFalse(mongoTestHelper.containRecord(removed));
 	}
 
 	@Test
-	public void testGetAllRegistrationITOneElement() {
+	public void testGetAllRegistrationITOneElement() throws IllegalJournalEntryException {
 		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		myDB.add(testEntry);
+		JournalEntry testEntry = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		mongoTestHelper.addRecord(testEntry);
 		assertEquals(1, myAccountantApp.getAllRegistration(dates[0], dates[1]).size());
 	}
 
 	@Test
-	public void testGetAllRegistrationITMoreElements() {
+	public void testGetAllRegistrationITMoreElements() throws IllegalJournalEntryException {
 		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry1 = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		JournalEntry testEntry2 = myAccountantApp.createJournalEntry("2", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		JournalEntry testEntry3 = myAccountantApp.createJournalEntry("3", dates[0],
-				GenericHelper.createTestList(1200, 1200));
-		myDB.add(testEntry1);
-		myDB.add(testEntry2);
-		myDB.add(testEntry3);
+		JournalEntry testEntry1 = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		JournalEntry testEntry2 = mongoTestHelper.createJournalEntry("2", 1200, 1200);
+		JournalEntry testEntry3 = mongoTestHelper.createJournalEntry("3", 1300, 1300);
+		mongoTestHelper.addRecord(testEntry1);
+		mongoTestHelper.addRecord(testEntry2);
+		mongoTestHelper.addRecord(testEntry3);
 		assertEquals(3, myAccountantApp.getAllRegistration(dates[0], dates[1]).size());
 	}
 
 	@Test
-	public void testGetAllRegistrationITOneBadElement() {
+	public void testGetAllRegistrationITOneBadElement() throws IllegalJournalEntryException {
 		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry1 = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		JournalEntry testEntry2 = myAccountantApp.createJournalEntry("2",
+		JournalEntry testEntry1 = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		JournalEntry testEntry2 = mongoTestHelper.createJournalEntry("2", 1200, 1200);
+		JournalEntry testEntry3 = myAccountantApp.createJournalEntry("3",
 				new Date(new GregorianCalendar(1910 + 100, 12, 11).getTimeInMillis()),
-				GenericHelper.createTestList(1300, 1300));
-		JournalEntry testEntry3 = myAccountantApp.createJournalEntry("3", dates[0],
-				GenericHelper.createTestList(1200, 1200));
-		myDB.add(testEntry1);
-		myDB.add(testEntry2);
-		myDB.add(testEntry3);
+				mongoTestHelper.createTestList(1300, 1300));
+		mongoTestHelper.addRecord(testEntry1);
+		mongoTestHelper.addRecord(testEntry2);
+		mongoTestHelper.addRecord(testEntry3);
 		assertEquals(2, myAccountantApp.getAllRegistration(dates[0], dates[1]).size());
 	}
 
 	@Test
-	public void testGetAllRegistrationITLostElement() {
+	public void testGetAllRegistrationITLostElement() throws IllegalJournalEntryException {
 		Date[] dates = authenticateAndCreateDates();
-		JournalEntry testEntry1 = myAccountantApp.createJournalEntry("1", dates[0],
-				GenericHelper.createTestList(1300, 1300));
-		JournalEntry testEntry2 = myAccountantApp.createJournalEntry("2", dates[0],
-				GenericHelper.createTestList(1100, 1100));
-		myDB.add(testEntry1);
-		myDB.add(testEntry2);
-		myDB.delete("2");
+		JournalEntry testEntry1 = mongoTestHelper.createJournalEntry("1", 1100, 1100);
+		JournalEntry testEntry2 = mongoTestHelper.createJournalEntry("2", 1200, 1200);
+		mongoTestHelper.addRecord(testEntry1);
+		mongoTestHelper.addRecord(testEntry2);
+		myAccountantApp.delete("2");
 		assertEquals(1, myAccountantApp.getAllRegistration(dates[0], dates[1]).size());
 	}
 
