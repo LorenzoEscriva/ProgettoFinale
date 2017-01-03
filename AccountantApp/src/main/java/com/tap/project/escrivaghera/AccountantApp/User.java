ecc58b6c43f25d4b6a7bc01286787a0bab5703cc
Matteo Ghera <matteo.ghera@stud.unifi.it>
2017-01-03 12:01:04 +0100

    user's documentation

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/User.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/User.java
@@ -1,10 +1,25 @@
 package com.tap.project.escrivaghera.AccountantApp;
 
+/**
+ * An instance of this class contains the information of user that can use this app
+ * 
+ * @author Matteo Ghera
+ * @author Lorenzo Escriva
+ *
+ */
 public class User {
 
 	private String id;
 	private String name;
 	private String surname;
+	
+	/**
+	 * Create a new instance of this class
+	 * 
+	 * @param associated user's id
+	 * @param associated user's name
+	 * @param associated user's surname
+	 */
 
 	public User(String id, String name, String surname) {
 		super();
@@ -13,6 +28,11 @@ public class User {
 		this.surname = surname;
 	}
 
+	/**
+	 * This method creates a description of the current object
+	 * 
+	 * @return the object's description
+	 */
 	@Override
 	public String toString() {
 		return id + " " + name + " " + surname;
