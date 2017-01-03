37828c5d1601c04852315105f194f0fa5a99f812
Matteo Ghera <matteo.ghera@stud.unifi.it>
2017-01-03 13:53:50 +0100

    added documentation to exception's classes

--- AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/exception/NotAuthenticationException.java
+++ AccountantApp/src/main/java/com/tap/project/escrivaghera/AccountantApp/exception/NotAuthenticationException.java
@@ -1,7 +1,17 @@
 package com.tap.project.escrivaghera.AccountantApp.exception;
 
+/**
+ * This is a subclass of RuntimeException's class. The methods throws this exception when they are called
+ * and the user not are authenticated
+ * @author Matteo Ghera
+ * @author Lorenzo Escriva
+ *
+ */
 public class NotAuthenticationException extends RuntimeException {
-
+	/**
+	 * The constructor of this class
+	 * @param error's description
+	 */
 	public NotAuthenticationException() {
 		super("Authentication missing");
 	}
